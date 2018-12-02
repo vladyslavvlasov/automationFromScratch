aef2704b37f35d121d5d7715fe6409909fa40f71
vladyslavvlasov@gmail.com <vladyslavvlasov@gmail.com>
2018-12-02 22:47:42 +0200

    add failed test

--- src/test/java/Slava/GoogleTest.java
+++ src/test/java/Slava/GoogleTest.java
@@ -10,10 +10,18 @@ public class GoogleTest extends BaseTest
 {
     private String expectedURL= "https://www.google.com.ua/";
 
+    private String expectedWrongURL= "https://www.google.com.ua2/";
+
     @Test
     public void openHomePage()
     {
         driver.get("https://www.google.com.ua");
         Assert.assertEquals(driver.getCurrentUrl(),expectedURL, "Expected current url to equal " + expectedURL);
+    }
+    @Test
+    public void openHomePageFail()
+    {
+        driver.get("https://www.google.com.ua");
+        Assert.assertEquals(driver.getCurrentUrl(),expectedWrongURL, "Expected current url to equal " + expectedURL);
     }
 }
